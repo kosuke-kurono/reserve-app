202685156bf57f04caf4f4788acd4058139b1b5d
kosuke-kurono <kurono.nexus6@gmail.com>
2022-06-19 08:10:45 +0900

    USERseeder関連を追加

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([[
+            'name' => 'admin',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'role' => 1
+            ],
+            [
+            'name' => 'manager',
+            'email' => 'manager@example.com',
+            'password' => Hash::make('password'),
+            'role' => 5
+            ],
+            [
+            'name' => 'test',
+            'email' => 'test@example.com',
+            'password' => Hash::make('password'),
+            'role' => 9
+            ]
+            ]); 
+    }
+}
